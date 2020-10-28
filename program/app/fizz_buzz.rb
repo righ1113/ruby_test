bcdeceab6c7e52364f7ee9bc1765b15db3eb8329
righ1113 <pykp42557@mineo.jp>
2020-10-29 01:50:59 +0900

    20/10/29 Bundler

--- program/app/fizz_buzz.rb
+++ program/app/fizz_buzz.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 # 単純な REPL ならこれで十分
-# $ ruby fizz_buzz.rb
+# $ bundle exec ruby fizz_buzz.rb
 # irb なら
-# $ irb
+# $ bundle exec irb
 # > load 'fizz_buzz.rb'
 # > fizz(50)
+
+require 'egison'
+
+# comment
+module Egi
+  # Rubyのmoduleのメソッドは普通に定義しただけじゃ、関数として使えないので
+  class << self
+    include Egison
+    def comb
+      match_all([1, 2, 3, 4, 5]) do
+        # x と y の隣を可変させている
+        with(List.(*_, _x, *_, _y, *_)) { [x, y] }
+      end
+    end
+  end
+end
 
 def fizz(nnn)
   (1..nnn).each do |k|
@@ -22,4 +38,7 @@ def fizz(nnn)
 end
 
 # メイン関数としたもの
-__FILE__ == $PROGRAM_NAME && fizz(50)
+if __FILE__ == $PROGRAM_NAME
+  fizz(50)
+  p Egi.comb
+end
